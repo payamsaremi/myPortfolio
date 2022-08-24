0c32a4a599d82e2780786c045ecf6ff5aed8bc9b
payam <payam.code@gmail.com>
2022-08-24 18:28:54 +0200

    fixing the side menue overlay peoblem

--- components/Navbar.js
+++ components/Navbar.js
@@ -27,7 +27,7 @@ export default function Navbar() {
   return (
     <nav className="flex flex-row items-center justify-between max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-5 sm:pb-16  text-gray-900 bg-opacity-60 dark:text-gray-100">
       <div className="ml-[-0.60rem]">
-        <MobileMenu />
+        {/* <MobileMenu /> */}
 
         <NavItem href="/" text="Home" />
         <NavItem href="/blog" text="Blog" />
