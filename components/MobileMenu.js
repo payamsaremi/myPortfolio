d1fbc645da67a69efe35b357784b1a3183837941
payam <payam.code@gmail.com>
2022-08-24 20:39:13 +0200

    fixing the side menue overlay peoblem

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -4,7 +4,7 @@ import { motion, useCycle, AnimatePresence } from "framer-motion";
 import { MenuToggle } from "./MobileMenueToggle";
 import { useDimensions } from "../hooks/useDimentions";
 import MobileMenuNavigation from "./MobileMenuNavigation";
-
+import NextLink from "next/link";
 export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => {
@@ -21,7 +21,7 @@ export default function MobileMenu() {
     closed: {
       opacity: 0,
       transition: {
-        staggerChildren: 0.2,
+        staggerChildren: 0.1,
         staggerDirection: -1,
         delay: 0.2,
       },
@@ -29,7 +29,7 @@ export default function MobileMenu() {
     open: {
       opacity: 1,
       transition: {
-        staggerChildren: 0.2,
+        staggerChildren: 0.1,
         staggerDirection: 1,
       },
     },
@@ -47,24 +47,34 @@ export default function MobileMenu() {
       <MenuToggle toggle={() => toggleOpen()} isOpen={isOpen} />
       <AnimatePresence>
         {isOpen && (
-          <motion.div
-            initial={"closed"}
-            animate={isOpen ? "open" : "closed"}
-            variants={menuVariants}
-            exit={"closed"}
-            className={"absolute z-50 top-60 p-2 bg-red-600"}
-          >
-            {links.map((link) => (
-              <motion.div
-                key={link.href}
-                variants={itemVariants}
-                whileHover={{ scale: 1.1 }}
-                className={"bg-green-600 cursor-pointer mb-2 px-5 py-2"}
-              >
-                {link.title}
-              </motion.div>
-            ))}
-          </motion.div>
+          <div>
+            <motion.div
+              initial={"closed"}
+              animate={isOpen ? "open" : "closed"}
+              variants={menuVariants}
+              exit={"closed"}
+              className={"absolute z-50 top-96 p-2 bg-gray-600 rounded-md"}
+            >
+              {links.map((link) => (
+                <motion.div
+                  key={link.href}
+                  variants={itemVariants}
+                  whileHover={{ scale: 1.1 }}
+                  className={
+                    "bg-gray-900 cursor-pointer mb-2 px-5 py-2 rounded-md"
+                  }
+                >
+                  <NextLink href={link.href}>{link.title}</NextLink>
+                </motion.div>
+              ))}
+            </motion.div>
+            <motion.div
+              onClick={() => toggleOpen()}
+              variants={menuVariants}
+              exit={"closed"}
+              className="fixed inset-0 z-20 backdrop-blur-sm transition-opacity"
+            />
+          </div>
         )}
       </AnimatePresence>
     </motion.nav>
