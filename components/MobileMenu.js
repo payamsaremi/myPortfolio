76b91bde25a6014b74da5a3c80bfc56537874114
payam <payam.code@gmail.com>
2022-08-25 00:06:27 +0200

    fixing the side menue overlay peoblem

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -6,6 +6,7 @@ import { useDimensions } from "../hooks/useDimentions";
 import MobileMenuNavigation from "./MobileMenuNavigation";
 import NextLink from "next/link";
 export default function MobileMenu() {
+  const { width, height } = useDimensions();
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => {
     setIsOpen(!isOpen);
@@ -17,11 +18,35 @@ export default function MobileMenu() {
     { href: "/projects", title: "Projects" },
   ];
 
+  const sidebar = {
+    open: (height = 1000) => ({
+      clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+      transition: {
+        type: "spring",
+        stiffness: 20,
+        restDelta: 2,
+        staggerChildren: 0.2,
+        staggerDirection: 1,
+      },
+    }),
+    closed: {
+      clipPath: "circle(30px at 40px 40px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+        staggerChildren: 0.2,
+        staggerDirection: -1,
+      },
+    },
+  };
+
   const menuVariants = {
     closed: {
       opacity: 0,
       transition: {
-        staggerChildren: 0.1,
+        staggerChildren: 0.2,
         staggerDirection: -1,
         delay: 0.2,
       },
@@ -47,35 +72,30 @@ export default function MobileMenu() {
       <MenuToggle toggle={() => toggleOpen()} isOpen={isOpen} />
       <AnimatePresence>
         {isOpen && (
-          <div>
-            <motion.div
-              initial={"closed"}
-              animate={isOpen ? "open" : "closed"}
-              variants={menuVariants}
-              exit={"closed"}
-              className={"absolute z-50 top-96 p-2 bg-gray-600 rounded-md"}
-            >
-              {links.map((link) => (
-                <motion.div
-                  key={link.href}
-                  variants={itemVariants}
-                  whileHover={{ scale: 1.1 }}
-                  className={
-                    "bg-gray-900 cursor-pointer mb-2 px-5 py-2 rounded-md"
-                  }
-                  onClick={() => toggleOpen()}
-                >
-                  <NextLink href={link.href}>{link.title}</NextLink>
-                </motion.div>
-              ))}
-            </motion.div>
-            <motion.div
-              onClick={() => toggleOpen()}
-              variants={menuVariants}
-              exit={"closed"}
-              className="fixed inset-0 z-20 backdrop-blur-sm transition-opacity"
-            />
-          </div>
+          <motion.div
+            initial={"closed"}
+            animate={isOpen ? "open" : "closed"}
+            variants={sidebar}
+            custom={height}
+            exit={"closed"}
+            className={
+              "absolute top-0 left-0 bottom-0 z-10 p-2 w-full bg-gray-800 overflow-hidden"
+            }
+          >
+            {links.map((link) => (
+              <motion.div
+                key={link.href}
+                variants={itemVariants}
+                whileHover={{ scale: 1.1 }}
+                className={`bg-gray-800 cursor-pointer mb-2 px-10 py-2 rounded-md `}
+                onClick={() => toggleOpen()}
+              >
+                <NextLink href={link.href}>
+                  <p className="text-xl">{link.title}</p>
+                </NextLink>
+              </motion.div>
+            ))}
+          </motion.div>
         )}
       </AnimatePresence>
     </motion.nav>
