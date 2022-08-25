9b7fe1f8e4cdcfc7a2076b2ba6f474c53bbc667b
payam <payam.code@gmail.com>
2022-08-25 20:03:50 +0200

    turn all images to next/future/image

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/future/image";
 export default function Avatar({ image, size }) {
   return (
     <Image className={"rounded-xl"} src={image} width={size} height={size} />
