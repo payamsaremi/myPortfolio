ea4ec474646beb9b1d75990c2301ede637a48e44
payam <payam.code@gmail.com>
2022-09-13 17:20:38 +0200

    added avatar to main page

--- components/BoxCard.js
+++ components/BoxCard.js
@@ -1,5 +1,4 @@
-import TitleAnimation from "../components/TitleAnimation";
-
+import Image from "next/future/image";
 import { FiArrowRight } from "react-icons/fi";
 import useHover from "../hooks/useHover";
 import NextLink from "next/link";
@@ -12,10 +11,17 @@ export default function BoxCard({ image, title, description, url = "/" }) {
         className="flex flex-col items-baseline justify-end border border-gray-800 rounded-md p-10 w-full min-h-[300px] cursor-pointer"
       >
         {image && (
-          <div className="bg-gray-800 w-24 h-24 rounded-full mb-5"></div>
+          <div className="bg-gray-800 w-24 h-24 rounded-full mb-5 ">
+            <Image
+              src={"/images/me-avatar.png"}
+              width={800}
+              height={800}
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
+          </div>
         )}
         <div className="ml-2 w-full">
-          <div className="font-normal text-4xl mb-2 z-0 text-gray-200">
+          <div className="font-normal text-4xl mb-2 z-0 dark:text-gray-200">
             {title}
           </div>
           <div className="flex w-full items-end justify-between">
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "media",
+  darkMode: "class",
   theme: {
     extend: {
       boxShadow: {
