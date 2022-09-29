464efdd9ed08c8e6da1e4161a9f4d7a7983eb39d
payam <payam.code@gmail.com>
2022-09-29 17:55:48 +0200

    making better seo robot.txt and sitemap

--- data/blog/api.js
+++ data/blog/api.js
@@ -37,7 +37,6 @@ export const getPostBySlug = (slug) => {
 export const getAllPosts = () => {
   const posts = getSlugs()
     .map((slug) => getPostBySlug(slug))
-    .filter((post) => post.meta.isPublic !== false) //make sure it is a public post
     .sort((a, b) => {
       const t1 = new Date(a.meta.date).getTime();
       const t2 = new Date(b.meta.date).getTime();
